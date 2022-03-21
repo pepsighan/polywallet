1cc8b963f3accbe00f9ffbe1ea76cf7470e0bf87
Sharad Chand <pepsighan@sharadchand.com>
2022-03-21 17:25:12 +0530

    Add store to manage wallet

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:polywallet/pages/intro.dart';
+import 'package:polywallet/store/wallet.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,26 +12,31 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Polywallet',
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-        useMaterial3: true,
-        appBarTheme: const AppBarTheme(elevation: 0),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ButtonStyle(
-            elevation: MaterialStateProperty.all(0),
-            minimumSize: MaterialStateProperty.all(const Size.fromHeight(44)),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => WalletState()),
+      ],
+      child: MaterialApp(
+        title: 'Polywallet',
+        theme: ThemeData(
+          primarySwatch: Colors.teal,
+          useMaterial3: true,
+          appBarTheme: const AppBarTheme(elevation: 0),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ButtonStyle(
+              elevation: MaterialStateProperty.all(0),
+              minimumSize: MaterialStateProperty.all(const Size.fromHeight(44)),
+            ),
+          ),
+          outlinedButtonTheme: OutlinedButtonThemeData(
+            style: ButtonStyle(
+              elevation: MaterialStateProperty.all(0),
+              minimumSize: MaterialStateProperty.all(const Size.fromHeight(44)),
+            ),
           ),
         ),
-        outlinedButtonTheme: OutlinedButtonThemeData(
-          style: ButtonStyle(
-            elevation: MaterialStateProperty.all(0),
-            minimumSize: MaterialStateProperty.all(const Size.fromHeight(44)),
-          ),
-        ),
+        home: const IntroPage(),
       ),
-      home: const IntroPage(),
     );
   }
 }
---
+++ lib/store/wallet.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+/// State for the current wallet of the app.
+class WalletState extends ChangeNotifier {}
