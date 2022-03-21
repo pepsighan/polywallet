0e75585ee8723bbcc23a1a1f0bc0b752f3f04b3b
Sharad Chand <pepsighan@sharadchand.com>
2022-03-21 17:20:46 +0530

    Define the theme for the app

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,21 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Polywallet',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.teal,
+        useMaterial3: true,
+        appBarTheme: const AppBarTheme(elevation: 0),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            elevation: MaterialStateProperty.all(0),
+            minimumSize: MaterialStateProperty.all(const Size.fromHeight(44)),
+          ),
+        ),
+        outlinedButtonTheme: OutlinedButtonThemeData(
+          style: ButtonStyle(
+            elevation: MaterialStateProperty.all(0),
+            minimumSize: MaterialStateProperty.all(const Size.fromHeight(44)),
+          ),
+        ),
       ),
       home: const IntroPage(),
     );
