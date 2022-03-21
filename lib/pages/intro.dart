4b66a9470056e2f1e13891243cc37ba17203b870
Sharad Chand <pepsighan@sharadchand.com>
2022-03-21 17:36:52 +0530

    Store the passphrase locally

--- lib/pages/intro.dart
+++ lib/pages/intro.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:polywallet/pages/onboarding.dart';
+import 'package:polywallet/store/wallet.dart';
 
-class IntroPage extends StatelessWidget {
+class IntroPage extends StatefulWidget {
   const IntroPage({Key? key}) : super(key: key);
 
   @override
+  State<IntroPage> createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+  late Future<void> _initialization;
+
+  @override
+  void initState() {
+    super.initState();
+    _initialization = WalletState.of(context).initializeState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // TODO: Routing based on whether a wallet is created or not.
-    return const OnboardingPage();
+    return FutureBuilder<void>(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          // TODO: Provide a proper loading indicator.
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        // TODO: Routing based on whether a wallet is created or not.
+        return const OnboardingPage();
+      },
+    );
   }
 }
--- lib/store/wallet.dart
+++ lib/store/wallet.dart
@@ -1,5 +1,10 @@
 import 'package:bip39/bip39.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:provider/provider.dart';
+
+const _secureStorage = FlutterSecureStorage();
+const _passphraseKey = 'walletPassphrase';
 
 /// State for the current wallet of the app.
 class WalletState extends ChangeNotifier {
@@ -8,17 +13,25 @@ class WalletState extends ChangeNotifier {
   /// The passphrase of the current wallet.
   String? get passphrase => _passphrase;
 
+  /// Initializes the state from the persisted state.
+  Future<void> initializeState() async {
+    _passphrase = await _secureStorage.read(key: _passphraseKey);
+    notifyListeners();
+  }
+
   /// Generates a passphrase for a new wallet.
-  void generateWalletPassphrase() {
+  Future<void> generateWalletPassphrase() async {
     _passphrase = generateMnemonic();
+    await _secureStorage.write(key: _passphraseKey, value: _passphrase);
     notifyListeners();
   }
 
   /// Restores a passphrase for an existing wallet.
-  void restoreWalletPassphrase(String passphrase) {
+  Future<void> restoreWalletPassphrase(String passphrase) async {
     final isValid = validateMnemonic(passphrase);
     if (isValid) {
       _passphrase = passphrase;
+      await _secureStorage.write(key: _passphraseKey, value: _passphrase);
       notifyListeners();
     }
   }
@@ -27,5 +40,10 @@ class WalletState extends ChangeNotifier {
   Future<void> logout() async {
     _passphrase = null;
     notifyListeners();
+  }
+
+  /// Gets the WalletState from the context.
+  static WalletState of(BuildContext context) {
+    return Provider.of<WalletState>(context, listen: false);
   }
 }
