7a5f821e12402446d10fe0755755f36247d233bf
Sharad Chand <pepsighan@sharadchand.com>
2022-03-21 17:30:50 +0530

    Add funcs to generate or restore wallet

--- lib/store/wallet.dart
+++ lib/store/wallet.dart
@@ -1,4 +1,31 @@
+import 'package:bip39/bip39.dart';
 import 'package:flutter/material.dart';
 
 /// State for the current wallet of the app.
-class WalletState extends ChangeNotifier {}
+class WalletState extends ChangeNotifier {
+  String? _passphrase;
+
+  /// The passphrase of the current wallet.
+  String? get passphrase => _passphrase;
+
+  /// Generates a passphrase for a new wallet.
+  void generateWalletPassphrase() {
+    _passphrase = generateMnemonic();
+    notifyListeners();
+  }
+
+  /// Restores a passphrase for an existing wallet.
+  void restoreWalletPassphrase(String passphrase) {
+    final isValid = validateMnemonic(passphrase);
+    if (isValid) {
+      _passphrase = passphrase;
+      notifyListeners();
+    }
+  }
+
+  /// Logs out and removes this wallet from the app.
+  Future<void> logout() async {
+    _passphrase = null;
+    notifyListeners();
+  }
+}
