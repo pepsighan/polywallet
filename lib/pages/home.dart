efcc596541cc9a8bbd907be6c1d7e06f9768f73d
Sharad Chand <pepsighan@sharadchand.com>
2022-03-21 21:37:17 +0530

    Add settings page to logout

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:polywallet/pages/settings.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,6 +9,17 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Polywallet'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SettingsPage()),
+              );
+            },
+            icon: const Icon(Icons.account_balance_wallet_rounded),
+          )
+        ],
       ),
     );
   }
---
+++ lib/pages/settings.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:polywallet/pages/intro.dart';
+import 'package:polywallet/store/wallet.dart';
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({Key? key}) : super(key: key);
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  void _onLogout() {
+    // Go to intro page once logged out.
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => const IntroPage()),
+      (route) => false,
+    );
+    WalletState.of(context).logout();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: ListView(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 16, left: 16, right: 16),
+            child: OutlinedButton(
+              onPressed: _onLogout,
+              child: const Text('Logout'),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 4, left: 16, right: 16),
+            child: Text(
+              'Once logged out, you will need your recovery phrase to setup '
+              'this wallet again.',
+              style: theme.textTheme.caption,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
