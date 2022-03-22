87a3e95710686e0a7f459ef3c80edc160de2a3ed
Sharad Chand <pepsighan@sharadchand.com>
2022-03-22 16:22:37 +0530

    Show send and receive buttons

--- lib/pages/token.dart
+++ lib/pages/token.dart
@@ -8,9 +8,74 @@ class TokenPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(token.asString),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 32),
+            child: const CircleAvatar(
+              child: Icon(Icons.currency_bitcoin, size: 32),
+              minRadius: 32,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 8),
+            child: Text(
+              '0 ${token.ticker}',
+              style: theme.textTheme.headline6,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 32),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(right: 8),
+                    child: OutlinedButton(
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Icon(Icons.arrow_upward),
+                          Container(
+                            margin: const EdgeInsets.only(left: 8),
+                            child: const Text('Send'),
+                          ),
+                        ],
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 8),
+                    child: OutlinedButton(
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Icon(Icons.arrow_downward),
+                          Container(
+                            margin: const EdgeInsets.only(left: 8),
+                            child: const Text('Receive'),
+                          ),
+                        ],
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
